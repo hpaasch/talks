954163463621e5e754cfaaca197f496bbab5c4fb
hpaasch <hepaasch@gmail.com>
2016-09-13 18:40:19 -0400

    templates roughed

--- pydata_talk/urls.py
+++ pydata_talk/urls.py
@@ -1,21 +1,16 @@
-"""pydata_talk URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
+from talk_app.views import IndexView, PrepareView, StateView, FederalView, ResourceView
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='index_view'),
+    url(r'^prepare/$', PrepareView.as_view(), name='prepare_view'),
+    url(r'^state/$', StateView.as_view(), name='state_view'),
+    url(r'^federal/$', FederalView.as_view(), name='federal_view'),
+    url(r'^resources/$', ResourceView.as_view(), name='resource_view'),
+
 ]
--- talk_app/views.py
+++ talk_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class PrepareView(TemplateView):
+    template_name = 'prepare.html'
+
+
+class FederalView(TemplateView):
+    template_name = 'federal.html'
+
+
+class StateView(TemplateView):
+    template_name = 'state.html'
+
+
+class ResourceView(TemplateView):
+    template_name = 'resource.html'
